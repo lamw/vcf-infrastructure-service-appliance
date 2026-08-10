bb123681def6889b4e6e94db8035eaaec03f25da
Josh Ghiloni <github@joshghiloni.me>
2026-08-10 12:07:11 -0400

    Cleanup cleanup script

--- scripts/vis-cleanup.sh
+++ scripts/vis-cleanup.sh
@@ -2,6 +2,9 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+echo '> Removing unused packages'
+sudo apt-get autoremove
+
 echo '> Clearing apt cache...'
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
@@ -13,7 +16,8 @@ sudo rm -rf /var/log/journal/* || true
 sudo rm -f /var/lib/dhcp/* || true
 
 echo '> Zeroing free space...'
-sudo dd if=/dev/zero of=/EMPTY bs=1M || true
+# Hide the dd error that will occur at the end of the command
+sudo dd if=/dev/zero of=/EMPTY bs=1M 2>/dev/null || true
 sudo sync
 sudo rm -f /EMPTY
 sudo sync
